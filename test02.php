3558fa65fdd290189bd94f0cec6cca4dcc25e217
comop <y.comop@gmail.com>
2020-07-14 22:35:41 +0900

    $operatorsの定義追加と戻るリンク設置

--- test02.php
+++ test02.php
@@ -1,30 +1,25 @@
 <?php
 $num01 = $_GET['num01'];
 $num02 = $_GET['num02'];
-if (is_numeric($num01) && is_numeric($num02)) {
-	switch ($operators) {
-		case "plus":
-			$answer = $num01 + $num02;
-<<<<<<< HEAD
-			bresk;
-		case "minus":
-			$answer = $num01 - $num02;
-			bresk;
-		case "times":
-			$answer = $num01 * $num02;
-			bresk;
-=======
-			break;
-		case "minus":
-			$answer = $num01 - $num02;
-			break;
-		case "times":
-			$answer = $num01 * $num02;
-			break;
->>>>>>> c47bc1cf5e420ddc092189dafa713ae2e6b98238
-	}
+$operators = $_GET['operators'];
+if (empty($num01) || empty($num02)) {
+	$answer = '数字を入力していない項目があります';
 } else {
-	$answer = '数字以外が含まれています';
+	if (is_numeric($num01) && is_numeric($num02)) {
+		switch ($operators) {
+			case "plus":
+				$answer = $num01 + $num02;
+				break;
+			case "minus":
+				$answer = $num01 - $num02;
+				break;
+			case "times":
+				$answer = $num01 * $num02;
+				break;
+		}
+	} else {
+		$answer = '数字以外が含まれています';
+	}
 }
 ?>
 
@@ -42,10 +37,12 @@ if (is_numeric($num01) && is_numeric($num02)) {
 <form action="test03.php" method="GET">
 <p>
 <input type="radio" name="answer" value="yes" required>はい
-<input type="radio" name="answer" value="yes">いいえ
+<input type="radio" name="answer" value="no">いいえ
 </p>
 <p><input type="submit" value="送信する"></p>
 </form>
+<br>
+<a href="test01.php">最初へ戻る</a>
 </body>
 </html>
 
