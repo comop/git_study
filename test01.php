ed275957e8f20550d9ebb72c014f6897d0096ae9
comop <y.comop@gmail.com>
2020-07-14 21:31:20 +0900

    四則演算子の追加

--- test01.php
+++ test01.php
@@ -9,13 +9,13 @@
 <form action="test02.php" method="GET">
 <p>
 数字1<input type="test" name="num01">
-</p>
-<p>
-+
-</p>
-<p>
+<select name="operators">
+<option value="plus">+</option>
+<option value="minus">-</option>
+<option value="times">x</option>
+<option value="divide">÷</option>
+</select>
 数字2<input type="text" name="num02">
-</p>
 <input type="submit" value="送信">
 </p>
 </form>
