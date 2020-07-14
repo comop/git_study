51131916b161bfd12a14c51180c8c7ffce1ba97e
comop <y.comop@gmail.com>
2020-07-14 21:14:15 +0900

    htmlレイアウト変更

--- test01.php
+++ test01.php
@@ -9,8 +9,13 @@
 <form action="test02.php" method="GET">
 <p>
 数字1<input type="test" name="num01">
+</p>
+<p>
 +
+</p>
+<p>
 数字2<input type="text" name="num02">
+</p>
 <input type="submit" value="送信">
 </p>
 </form>
